b954f4bbc1615518fbb54682f418542d124fa68c
ADINA\Adina-PC <adinajarda2@gmail.com>
2023-10-11 19:48:33 +0300

    laborator 2

---
+++ lab2/src/lab2/MsinClass.java
@@ -0,0 +1,32 @@
+  package lab2;
+import java.util.Scanner;
+public class MsinClass {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int inputValue=input.nextInt();
+		
+//		System.out.println("valoarea citita de la tastatura este " +inputValue);
+		
+		int maxInt=Integer.MAX_VALUE;
+		System.out.println(maxInt + 1);
+		
+		int minInt=Integer.MIN_VALUE;
+		System.out.println(minInt - 1);
+		
+		float maxFloat=Float.MAX_VALUE;
+		System.out.println(maxFloat * 3);
+		
+		try
+		{
+			System.out.println(maxInt/inputValue);
+		}
+			catch(ArithmeticException exception)
+		{
+				System.out.println("ai intodus o valoare gresita, introdu alta valoare ");
+				inputValue=input.nextInt();
+		}
+		System.out.println("noua valoare: "+inputValue);
+	}
+
+}
