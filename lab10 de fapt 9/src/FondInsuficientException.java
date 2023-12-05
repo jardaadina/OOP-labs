253b6f62ccd8e0aa6de8c7a35006cb1e4729f629
ADINA\Adina-PC <adinajarda2@gmail.com>
2023-12-05 23:12:41 +0200

    Revert "lab 9+ problema 6.1 lasata sa o rezolvam"
    This reverts commit a2240189f4afbc62f8eda2d9917bd096fd4bba2a.

--- lab10 de fapt 9/src/FondInsuficientException.java
+++
@@ -1,6 +0,0 @@
-public class FondInsuficientException extends Exception {
-    public FondInsuficientException(String msg){
-        super(msg);
-        System.out.println(msg);
-    }
-}
--- problema6.1/src/ExceptieZiNelucratoare.java
+++
@@ -1,6 +0,0 @@
-public class ExceptieZiNelucratoare extends Throwable {
-    public ExceptieZiNelucratoare(String msg) {
-        super(msg);
-        System.out.println(msg);
-    }
-}
